13a5fdd8361d7c965d9295e11dec2a0058ebe883
jackgrahamhindley <62214824+jackgrahamhindley@users.noreply.github.com>
2021-01-05 19:02:57 +0000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,15 +1,36 @@
-from mycroft import MycroftSkill, intent_file_handler
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill, intent_handler, intent_file_handler
+from mycroft.util import LOG
+from requests import post
+import time
+from pixel_ring import pixel_ring
+from gpiozero import LED
+
+power = LED(5)
+power.on()
+
+pixel_ring.set_brightness(10)
 
 
 class Rgblisten(MycroftSkill):
     def __init__(self):
-        MycroftSkill.__init__(self)
+        super(Rgblisten, self).__init__(name="Rgblisten")
+        
+    def initialize(self):
+        self.add_event('recognizer_loop:record_begin', self.record_begin_handler)
+        
 
-    @intent_file_handler('rgblisten.intent')
-    def handle_rgblisten(self, message):
-        self.speak_dialog('rgblisten')
+    def record_begin_handler(self, message):
+        pixel_ring.wakeup()
+        time.sleep(2)
+        pixel_ring.think()
+        time.sleep(2)
+        pixel_ring.speak()
+        time.sleep(3)
+        pixel_ring.off()
+        time.sleep(2)
+        pixel_ring.off()
 
-
+    
 def create_skill():
     return Rgblisten()
-
